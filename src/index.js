99e4f4233bc024ae03d8eacb4f6ffa7a58421e04
Anna <michalowska.aneczka@gmail.com>
2023-01-26 21:50:00 +0100

    first_commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
 import './index.css';
+
+import { Statistics } from 'components/Statistics/Statistics';
+import { Profile } from 'components/Profile/Profile';
+import { FriendList } from 'components/FriendList/FriendList';
+import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
+
+import data from 'components/Data/data.json';
+import user from 'components/Data/user.json';
+import friends from 'components/Data/friends.json';
+import transactions from 'components/Data/transactions.json';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
+    <Statistics title="Upload stats" stats={data} />
+    <FriendList friends={friends} />
+    <TransactionHistory items={transactions} />
   </React.StrictMode>
 );
